3bbf6a39ac30588ff995f4ca07f2ad834eada82c
tyler finley & lisa folliard <student@epicodus.com>
2015-08-10 14:27:31 -0700

    Add weight parameter, add cost to ship method.

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,12 +1,16 @@
 class Parcels
-  define_method(:initialize) do |length, width, height|
+  define_method(:initialize) do |length, width, height, weight|
     @length = length
     @width = width
     @height = height
+    @weight = weight
   end
 
 
   define_method(:volume) do
     @length.*(@width).*(@height)
     end
+  define_method(:cost_to_ship) do
+    @length.*(@width).*(@height).+(@weight)
+  end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -3,7 +3,11 @@ require('parcels')
 
 describe('Fixnum#parcels') do
   it("returns the volume of a parcel") do
-    parcel_volume = Parcels.new(3,3,3)
+    parcel_volume = Parcels.new(3,3,3,0)
     expect(parcel_volume.volume()).to(eq(27))
+  end
+  it("returns the weight + volume of the parcel") do
+    cost_to_ship = Parcels.new(3,3,3,20)
+    expect(cost_to_ship.cost_to_ship()).to(eq(47))
   end
 end
